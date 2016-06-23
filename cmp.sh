501d98db8e366122ba3d3c35b2be2def3fcc7723
MajorTom327 <majortom@d-society.fr>
2016-06-24 01:32:30 +0200

    apply a update, just some fix to moulitest needed

--- cmp.sh
+++ cmp.sh
@@ -1,23 +1,67 @@
 #!/bin/zsh
 rm *.o test_gnl
+cp ../main.c ./
+cp ../debug.h ./
 clear
 make -C ./libft fclean && make -C ./libft
-echo "Libft OK"
+echo "Libft \t\t\033[32m[OK]\033[0m"
 echo ""
 clang -Wall -Wextra -Werror -I ./libft/includes -o get_next_line.o -c get_next_line.c
 clang -Wall -Wextra -Werror -I ./libft/includes -o main.o -c main.c
-echo "Sources files OK"
+echo "Sources files \t\033[32m[OK]\033[0m"
 echo ""
 clang -o test_gnl main.o get_next_line.o libdbg.a -I libft/includes -L libft/ -lft
-echo "Executable OK"
+echo "Executable \t\033[32m[OK]\033[0m"
 echo ""
 echo ""
 
 echo -n "12345678" > test
-#cat -e test
-echo "Without EOL"
-./test_gnl
+echo "\033[32mWithout EOL\033[0m"
+./test_gnl | cat -e
+
 echo "12345678" > test
-#cat -e test
-echo "With EOL"
-./test_gnl
+echo "\033[32mWith EOL\033[0m"
+./test_gnl | cat -e
+
+echo ""
+
+echo "123456789" > test
+echo "\033[32m123456789\033[0m"
+./test_gnl | cat -e
+
+echo ""
+
+echo "1234567812345678" > test
+echo "\033[32m1234567812345678\033[0m"
+./test_gnl | cat -e
+
+echo ""
+
+echo "123456789" > test
+echo "ABCDEFGHI" >> test
+echo "\033[32m123456789#ABCDEFGH\033[0m"
+./test_gnl | cat -e
+
+echo ""
+
+echo "\033[32m1234\033[0m"
+echo "1234" > test
+./test_gnl | cat -e
+
+echo ""
+
+echo "1234" > test
+echo "abcd" >> test
+echo "5678" >> test
+echo "efgh" >> test
+echo "\033[32mSquare\033[0m"
+./test_gnl | cat -e
+
+echo ""
+
+echo "abcdefghijklmnop" > test
+echo "\033[32mabcdefghijklmnop\033[0m"
+./test_gnl | cat -e
+
+rm main.c
+rm *.o test_gnl test debug.h
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/10 00:43:18 by vthomas           #+#    #+#             */
-/*   Updated: 2016/06/18 11:14:19 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/06/24 01:21:50 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 #include "get_next_line.h"
 #include "libft/libft.h"
+
+static void	sf_addtostr(char **dst, const char *src)
+{
+	char *tmp;
+
+	tmp = ft_strdup(*dst);
+	ft_strdel(dst);
+	*dst = ft_strnew(ft_strlen(tmp) + ft_strlen(src));
+	ft_strcat(*dst, tmp);
+	ft_strcat(*dst, src);
+	ft_strdel(&tmp);
+}
 
 static void	sf_repstart(char **str)
 {
@@ -36,7 +48,7 @@ static int	sf_save(char **str)
 	{
 		if (save == NULL)
 		{
-			*str = ft_strnew(1);
+			*str = ft_strnew(0);
 			return (0);
 		}
 		*str = ft_strdup(save);
@@ -49,21 +61,8 @@ static int	sf_save(char **str)
 		ft_strdel(&save);
 		return (0);
 	}
-	ft_strdel(&save);
 	save = ft_strdup(*str);
 	return (0);
-}
-
-static void	sf_addtostr(char **dst, const char *src)
-{
-	char *tmp;
-
-	tmp = ft_strdup(*dst);
-	ft_strdel(dst);
-	*dst = ft_strnew(ft_strlen(tmp) + ft_strlen(src));
-	ft_strcat(*dst, tmp);
-	ft_strcat(*dst, src);
-	ft_strdel(&tmp);
 }
 
 int			get_next_line(const int fd, char **line)
@@ -73,7 +72,7 @@ int			get_next_line(const int fd, char **line)
 
 	if (fd == -1 || line == NULL)
 		return (-1);
-	ft_strdel(line);
+	*line = NULL;
 	if (sf_save(line))
 		return (1);
 	ret = BUFF_SIZE;
