6af0c5eef59263464bfb1c6f55c0b5a4276eee00
MajorTom327 <majortom@d-society.fr>
2016-08-14 04:45:23 +0200

    Show current File and change chmod

--- dir.sh
+++ dir.sh
@@ -11,7 +11,7 @@ $COUNT = 0
 for FILE in $( ls $DIR)
 do
 	printf "\n \e[36m[ Running... ]\n\n\e[33m"
-	printf "\e[2J\e[0;0H\e[90m[\e[32m%d\e[90m / \e[33m32\e[90m]\e[0m" "$COUNT" >> /dev/ttys005
+	printf "\e[2J\e[0;0H\e[90m[\e[32m$COUNT\e[90m / \e[33m32\e[90m]\e[0m\n\e[90m[ $FILE ]\e[0m\n" >> /dev/ttys005
 	let "COUNT++"
 	time ./test_gnl $DIR/$FILE > out
 	printf "\n   \e[32m[ CORRECT OUTPUT ]                    \e[31m[ GNL OUTPUT ]\n\n\e[33m"
